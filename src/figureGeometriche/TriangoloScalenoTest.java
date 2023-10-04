1b721e1b80d7ce1dd748c98ebc96e321cff7f19d
luca.tamanini <luca.tamanini@AULAS51-PC13.itib.local>
2023-10-04 10:16:08 +0200

    si.

--- src/figureGeometriche/TriangoloScalenoTest.java
+++ src/figureGeometriche/TriangoloScalenoTest.java
@@ -6,13 +6,18 @@ public class TriangoloScalenoTest{
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        
         float lato1, lato2, lato3, perimetro;
+        
         double area;
-        System.out.println("inserisci il valore del lato 1: ");
+        
+        System.out.print("inserisci il valore del lato 1: ");
         lato1 = in.nextFloat();
-        System.out.println("inserisci il valore del lato 2: ");
+        
+        System.out.print("inserisci il valore del lato 2: ");
         lato2 = in.nextFloat();
-        System.out.println("inserisci il valore del lato 3: ");
+        
+        System.out.print("inserisci il valore del lato 3: ");
         lato3 = in.nextFloat();
 
         TriangoloScaleno ts = new TriangoloScaleno(lato2, lato1, lato3);
