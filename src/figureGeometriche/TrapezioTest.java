99e77136a9c69abccae16775f0983b9ad0c5c514
lucatama <luca.tamanini@buonarroti.tn.it>
2023-10-18 17:50:37 +0200

    "si cosi"

--- src/figureGeometriche/TrapezioTest.java
+++ src/figureGeometriche/TrapezioTest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class TrapezioTest {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     double base1, base2, altezza, lato, area, perimetro;
