a87d2e7b0cf83f837644fa112fb07d3a58eeae38
luca.tamanini <luca.tamanini@AULAS51-PC20.itib.local>
2023-11-15 10:16:46 +0100

    da finire + fare Numero1Test

---
+++ src/numero/Numero1.java
@@ -0,0 +1,59 @@
+/*
+costruire una classe che, dato un numero intero, stabilisca se è pari o dispari
+e se maggiore, minore o uguale a 0
+*/
+package numero;
+
+public class Numero1 {
+    private int n1;
+    
+    public Numero1(){
+        
+    }
+    
+    public Numero1(int n1){
+        this.n1=n1;
+    }
+
+    public int getN1() {
+        return n1;
+    }
+
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+    
+    public String info(){
+        String testo = "il numero è: " + n1 + "\n";
+        return testo;
+    }
+    
+    public String pariDispari(){
+        
+        String testo;
+        
+        if(n1%2==0){
+            testo="pari";
+        }else{
+            testo="dispari";
+        }
+        
+        return testo;
+    }
+    
+    public String magMinUg(){
+        String testo;
+        
+        if (n1>0){
+            testo="maggiore di 0";
+        }else{
+            if (n1<0){
+                testo="minore di 0";
+            }else{
+                testo="uguale a 0";
+            }
+        }
+        
+        return testo;
+    }
+}
