7aeed34bd7bce28688d0eef9070dea7cec4e73fb
lucatama <luca.tamanini@buonarroti.tn.it>
2024-01-10 17:36:26 +0100

    fatto i compiti per mercoledi 17/01/2024

--- src/meteo/Meteo.java
+++ src/meteo/Meteo.java
@@ -66,7 +66,7 @@ public class Meteo {
         if (temperatura == 0) {
             testo = "si va a sciare";
         }
-        if (temperatura > 0 && temperatura < 5) {
+        if (temperatura >= 1 && temperatura <= 5) {
             testo = "ci vuole il cappellino";
         }
         if (temperatura > 5 && temperatura <= 10) {
@@ -76,31 +76,39 @@ public class Meteo {
             testo = "solo il maglioncino";
         }
         if (temperatura > 15 && temperatura <= 20) {
-            testo = "magari!";
+            testo = "magari";
         }
         if (temperatura > 20 && temperatura <= 25) {
-            testo = "condizionatore a manetta!";
+            testo = "tutti al mare";
+        }
+        if(temperatura>25){
+            testo = "condizionatore a manetta";
         }
 
         return testo;
     }
 
+    /**
+     * @return String
+     */
     public String consiglio2() {
         String testo = "";
         if (temperatura < 0) {
             testo = "attenti al ghiaccio";
         } else if (temperatura == 0) {
             testo = "si va a sciare";
-        } else if (temperatura > 0 && temperatura < 5) {
+        } else if (temperatura >= 1 && temperatura <= 5) {
             testo = "ci vuole il cappellino";
         } else if (temperatura > 5 && temperatura <= 10) {
             testo = "basta il piumino";
         } else if (temperatura > 10 && temperatura <= 15) {
             testo = "solo il maglioncino";
         } else if (temperatura > 15 && temperatura <= 20) {
-            testo = "magari!";
+            testo = "magari";
         } else if (temperatura > 20 && temperatura <= 25) {
-            testo = "condizionatore a manetta!";
+            testo = "tutti al mare";
+        }else{
+            testo = "condizionatore a manetta";
         }
         return testo;
     }
@@ -108,32 +116,34 @@ public class Meteo {
     public String consiglio3() {
         String testo = "";
 
-        if (temperatura > 0){
-            if(temperatura>=1){
-                if(temperatura>=5){
-                    if (temperatura>=10){
-                        if (temperatura>=15){
-                            if (temperatura>=20){
-                                if (temperatura>25){
-                                    testo = "condizionatore a manetta!";
+        if (temperatura >= 0) {
+            if (temperatura >= 1) {
+                if (temperatura > 5) {
+                    if (temperatura > 10) {
+                        if (temperatura > 15) {
+                            if (temperatura > 20) {
+                                if (temperatura > 25) {
+                                    testo = "condizionatore a manetta";
+                                }else{
+                                    testo = "tutti al mare";
                                 }
-                            }else{
-                                testo = "tutti al mare!";
+                            } else {
+                                testo = "magari";
                             }
-                        }else{
-                            testo = "magari!";
+                        } else {
+                            testo = "solo li maglioncino";
                         }
-                    }else{
-                        testo = "solo il maglioncino";
+                    } else {
+                        testo = "basta il piumino";
                     }
-                }else{
-                    
+                } else {
+                    testo = "ci vuole il cappellino";
                 }
-            }else{
-                
+            } else {
+                testo = "si va a sciare";
             }
-        }else{
-            
+        } else {
+            testo = "attenti al ghiaccio";
         }
         return testo;
     }
@@ -141,6 +151,35 @@ public class Meteo {
     public String consiglio4() {
         String testo = "";
 
+        if (temperatura <= 25) {
+            if (temperatura <= 20) {
+                if (temperatura <= 15) {
+                    if (temperatura <= 10) {
+                        if (temperatura < 5) {
+                            if (temperatura < 1) {
+                                if (temperatura < 0) {
+                                    testo = "attenti al ghiaccio";
+                                }else{
+                                    testo = "si va a sciare";
+                                }
+                            } else {
+                                testo = "ci vuole il cappellino";
+                            }
+                        } else {
+                            testo = "basta il piumino";
+                        }
+                    } else {
+                        testo = "solo il maglioncino";
+                    }
+                } else {
+                    testo = "magari";
+                }
+            } else {
+                testo = "tutti al mare";
+            }
+        } else {
+            testo = "condizionatore a manetta";
+        }
         return testo;
     }
 
---
+++ src/meteo/MeteoTest.java
@@ -0,0 +1,20 @@
+package meteo;
+import java.util.Scanner;
+public class MeteoTest{
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int temperatura;
+
+        System.out.print("inserisci la temperatura: ");
+        temperatura = in.nextInt();
+
+        Meteo meteo = new Meteo(temperatura);
+
+        System.out.println(meteo.consiglio());
+        System.out.println(meteo.consiglio2());
+        System.out.println(meteo.consiglio3());
+        System.out.println(meteo.consiglio4());
+    }
+}
