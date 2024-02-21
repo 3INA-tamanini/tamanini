464d38739d95b0cc2c063cecd294f56b31ca5946
luca.tamanini <luca.tamanini@AULAS51-PC20.itib.local>
2024-02-21 10:12:37 +0100

    zeller's congruence

---
+++ src/Data/Zeller.java
@@ -0,0 +1,49 @@
+package Data;
+
+public class Zeller {
+
+    public static String zellerCongruence(int d, int m, int y) {
+        String testo = "";
+        if (m == 1) {
+            m = 13;
+            y--;
+        }
+        if (m == 2) {
+            m = 14;
+            y--;
+        }
+        int q = d;
+        int k = y % 100;
+        int j = y / 100;
+        int h = q + 13 * (m + 1) / 5 + k + k / 4 + j / 4 + 5 * j;
+        h = h % 7;
+        switch (h) {
+            case 0:
+                testo = "Saturday";
+                break;
+            case 1:
+                testo = "Sunday";
+                break;
+            case 2:
+                testo = "Monday";
+                break;
+            case 3:
+                testo = "Tuesday";
+                break; 
+           case 4:
+                testo = "wednesday";
+                break;
+            case 5:
+                testo = "Thursday";
+                break;
+            case 6:
+                testo = "friday";
+                break;
+        }
+        return testo;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(zellerCongruence(1, 4, 2000));
+    }
+}
