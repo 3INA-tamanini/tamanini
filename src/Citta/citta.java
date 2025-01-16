6335ed4973d919a8ac803f8d592fa32900c0e723
lucatama <luca.tamanini@buonarroti.tn.it>
2025-01-16 21:34:59 +0100

    citta non va il main

--- src/Citta/citta.java
+++ src/Citta/citta.java
@@ -1,18 +1,18 @@
-package Citta;
+package citta;
 
 import persona7.Persona7;
 
-public class citta {
+public class Citta {
 
-    String nome;
-    String superficie;
-    Persona7 sindaco;
-    Persona7[] abitanti;
+    private String nome;
+    private String superficie;
+    private Persona7 sindaco;
+    private Persona7[] abitanti;
 
-    public citta() {
+    public Citta() {
     }
 
-    public citta(String nome, String superficie, Persona7 sindaco, Persona7[] abitanti) throws Exception {
+    public Citta(String nome, String superficie, Persona7 sindaco, Persona7[] abitanti) throws Exception {
         setNome(nome);
         setSuperficie(superficie);
         setSindaco(sindaco);
@@ -24,20 +24,19 @@ public class citta {
     }
 
     public void setNome(String nome) throws Exception {
-        if (nome == null) {
-            throw new Exception("il nome è null");
-        }
-        if (nome.length() <= 0) {
-            throw new Exception("il nome deve essere maggiore di 0");
-        }
-        if (Character.isUpperCase(nome.charAt(0))) {
-            throw new Exception("la prima lettera del nome deve essere maiuscolo");
-        }
-        if (nome.matches("\\D+")) {
+        if (nome != null) {
+            if (Character.isUpperCase(nome.charAt(0)) == true) {
+                if (nome.matches("\\D+")) {
+                    this.nome = nome;
+                } else {
+                    throw new Exception("Il nome della città deve contenere SOLO lettere");
+                }
+            } else {
+                throw new Exception("La prima lettera del nome della città DEVE essere maiuscola");
+            }
         } else {
-            throw new Exception("il nome della città non può avere numeri");
+            throw new Exception("Il nome della città NON deve essere null");
         }
-        this.nome = nome;
     }
 
     public String getSuperficie() {
@@ -45,14 +44,15 @@ public class citta {
     }
 
     public void setSuperficie(String superficie) throws Exception {
-        if (superficie == null) {
-            throw new Exception("la stringa non può essere null");
+        if (superficie != null) {
+            if (superficie.matches("[0-9\\,]+[km,m]+")) {
+                this.superficie = superficie;
+            } else {
+                throw new Exception("Scrivere prima il numero e dopo l'untità di misura");
+            }
+        } else {
+            throw new Exception("La superficie NON deve essere null");
         }
-        if (!superficie.matches("[0-9\\,\\.]+[km,m]+")) {
-            throw new Exception("la superficie deve avere un numero e poi unita di misura");
-        }
-
-        this.superficie = superficie;
     }
 
     public Persona7 getSindaco() {
@@ -60,24 +60,81 @@ public class citta {
     }
 
     public void setSindaco(Persona7 sindaco) throws Exception {
-        if(sindaco==null)
-            throw new Exception("sindaco null");
-        this.sindaco = sindaco;
+        if (sindaco != null) {
+            this.sindaco = sindaco;
+        } else {
+            throw new Exception("Il nome del sindaco NON può essere null");
+        }
     }
 
     public Persona7[] getAbitanti() {
         return abitanti;
     }
 
-    public void setAbitanti(Persona7[] abitanti) throws Exception {
-        if(abitanti == null)
-            throw new Exception("abitante null");
-        
-        for (int i = 0; i < abitanti.length; i++) {
-            this.abitanti[i] = abitanti[i];
-            
+    public void setAbitanti(Persona7[] abitanti) {
+        this.abitanti = new Persona7[abitanti.length + 1];
+        this.abitanti[0] = sindaco;
+        for (int i = 1; i < abitanti.length; i++) {
+            this.abitanti[i] = abitanti[i - 1];
         }
-        
     }
 
+    public Integer densitaPopolazione() {
+        Integer dens = 0;
+        int temp = superficie.indexOf(',');
+        String s = superficie.substring(0, temp - 1);
+
+        dens = abitanti.length / Integer.parseInt(s);
+
+        return dens;
+    }
+
+    public Integer etaMedia() throws Exception {
+        Integer eta = 0;
+
+        for (int i = 0; i < abitanti.length; i++) {
+            eta += abitanti[i].calcolaEta();
+        }
+       
+        eta /= abitanti.length;
+
+        return eta;
+    }
+
+    public String info() {
+        String testo = "";
+        for (int i = 0; i < abitanti.length; i++) {
+            testo += getAbitanti()[i].info();
+        }
+        return testo;
+    }
+
+    public Integer numeroOmonimi() throws Exception{
+        Integer numero = 0;
+        for(int i = 0; i<abitanti.length;i++){
+            for (int j = 0; j < abitanti.length; j++) {
+                if(abitanti[i].verificaOmonimia(abitanti[j])){
+                    numero++;
+                }
+            }
+        }
+        return numero;
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Persona7 sindaco = new Persona7(1.0, "Pompilio", "Abitatni", 123f, "12/12/1912", "matteo.pompilio@cico.com", "Ad345678!");
+            Persona7 p1 = new Persona7(1.0, "Ghetta", "Abitatni", 123f, "12/12/1912", "matteo.pompilio@cico.com", "C23456a8!");
+            Persona7 p2 = new Persona7(1.0, "Tamaontop", "Abitatni", 123f, "12/12/1912", "matteo.pompilio@cico.com", "D2s45678!");
+            Persona7 p3 = new Persona7(1.0, "Tomaonline", "Abitatni", 123f, "12/12/1912", "matteo.pompilio@cico.com", "C1a0B3l0!");
+            Persona7[] abitanti = {p1, p2, p3, sindaco};
+            Citta c = new Citta("Trento", "157,9km", sindaco, abitanti);
+            System.out.println(c.info());
+            
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
