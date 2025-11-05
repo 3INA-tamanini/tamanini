25de7a6ec7ea8ca2ef62a5789fc27ddd386273c5
luca.tamanini <luca.tamanini@AULAS51-PC25.itib.local>
2025-11-05 10:14:54 +0100

    esercizi in classe, svuota e elimina da finire

---
+++ listaSpesa/script.js
@@ -0,0 +1,46 @@
+orso = document.querySelector("#cisco");
+bottone = document.querySelector("#panda");
+bottone.addEventListener("click", invia);
+const lista = document.querySelector("#lista");
+svuota = document.querySelector("#svuota");
+elimina = document.querySelector("#elimina");
+
+svuota.addEventListener("click", svuotaEl);
+elimina.addEventListener("click", elimEl);
+
+function elimEl(){
+    const temp = document.querySelectorAll("");
+    temp.remove;
+}
+
+function svuotaEl(){
+    lista.remove;
+}
+function invia() {
+    event.preventDefault();
+
+    if (!(orso.value === "")) {
+        let nuovo = document.createElement("li");
+        nuovo.textContent = orso.value;
+
+        let comprato = document.createElement("button");
+        comprato.textContent = "comprato";
+
+        let rimuovi = document.createElement("button");
+        rimuovi.textContent = "rimuovi";
+
+        nuovo.appendChild(comprato);
+        comprato.addEventListener("click", () => {
+            nuovo.classList.toggle("comprato");
+        });
+
+        nuovo.appendChild(rimuovi);
+        rimuovi.addEventListener("click", ()=>{
+            nuovo.remove();
+        });
+        
+        lista.appendChild(nuovo);
+    } else {
+        alert("inserire un elemento");
+    }
+}
