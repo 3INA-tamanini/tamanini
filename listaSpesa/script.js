9005581e55a7017b78d810bb03afe639b31dbfe3
luca.tamanini <luca.tamanini@AULAS51-PC25.itib.local>
2025-11-12 10:17:42 +0100

    updatare lista spesa

--- listaSpesa/script.js
+++ listaSpesa/script.js
@@ -1,30 +1,68 @@
 orso = document.querySelector("#cisco");
 bottone = document.querySelector("#panda");
-bottone.addEventListener("click", invia);
+bottone.addEventListener("click", aggiungiEl);
 const lista = document.querySelector("#lista");
 svuota = document.querySelector("#svuota");
 elimina = document.querySelector("#elimina");
-
 svuota.addEventListener("click", svuotaEl);
 elimina.addEventListener("click", elimEl);
+let array = JSON.parse(localStorage.getItem("spesa")) || ["pane"];
+
+inviaArray();
 
 function elimEl() {
     const temp = document.querySelectorAll(".comprato");
 
-    temp.forEach((element) => {
-        element.remove();
-    });
+    temp.forEach((element) => element.remove());
 }
 
 function svuotaEl() {
     lista.innerHTML = "";
+
+    localStorage.removeItem("spesa");
 }
-function invia() {
+
+
+/*if (localStorage.getItem("spesa") !== null) {
+    inviaArray();
+} else {
+    invia();
+}
+*/
+function inviaArray() {
+    array.forEach(e=>{
+        let nuovo = document.createElement("li");
+        nuovo.textContent = e + " ";
+        let comprato = document.createElement("button");
+        comprato.textContent = "comprato";
+
+        let rimuovi = document.createElement("button");
+        rimuovi.textContent = "rimuovi";
+
+        nuovo.appendChild(comprato);
+        comprato.addEventListener("click", () => {
+            nuovo.classList.toggle("comprato");
+        });
+
+        nuovo.appendChild(rimuovi);
+        rimuovi.addEventListener("click", () => {
+            nuovo.remove();
+        });
+        lista.appendChild(nuovo);
+    })
+    
+}
+
+function aggiungiEl(event) {
     event.preventDefault();
 
     if (!(orso.value === "")) {
         let nuovo = document.createElement("li");
-        nuovo.textContent = orso.value;
+        nuovo.textContent = orso.value + " ";
+        array.push(nuovo.textContent);
+        console.log(array);
+
+        localStorage.setItem("spesa", JSON.stringify(array));
 
         let comprato = document.createElement("button");
         comprato.textContent = "comprato";
@@ -41,9 +79,9 @@ function invia() {
         rimuovi.addEventListener("click", () => {
             nuovo.remove();
         });
-
         lista.appendChild(nuovo);
     } else {
         alert("inserire un elemento");
     }
+
 }
